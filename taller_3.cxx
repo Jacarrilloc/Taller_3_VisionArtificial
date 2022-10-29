773365dd8bfb9728901c1387d238b8b43e3881f7
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-10-29 07:35:05 -0500

    Adelantos

--- taller_3.cxx
+++ taller_3.cxx
@@ -9,6 +9,25 @@
 using namespace std;
 using namespace cv;
 
+void guardarImagen(Mat imagen,String nombre)
+{
+  imwrite( nombre + ".png", imagen );
+  cout<<"-----IMAGEN " << nombre << ".png CREADA----"<<endl; 
+}
+
+void opcionManual(Mat src)
+{
+  Mat mask;
+  inRange(src, Scalar(255, 255, 255), Scalar(255, 255, 255), mask);
+  src.setTo(Scalar(0, 0, 0), mask);
+  guardarImagen(src,"Sinfondo");
+}
+
+void opcionAuto()
+{
+
+}
+
 int main( int argc, char** argv )
 {
     //-----LECTURA DE IMAGEN-----
@@ -32,6 +51,36 @@ int main( int argc, char** argv )
     std::cerr << "Error: No image data" << std::endl;
     return( -1);
   }
-
   //Inicio de Programa
+
+  int opcion;
+
+    Mat imagen_gris;
+
+  cv::cvtColor(image,imagen_gris, cv::COLOR_BGR2GRAY);
+  guardarImagen(imagen_gris,"engris");
+
+  while (opcion != 3)
+  { 
+
+  cout<<"SELECCIONE UNA OPCION:"<<endl;
+  cout<<"1.Semilla de forma Manual"<<endl;
+  cout<<"2.Semilla de forma Automatica"<<endl;
+  cout<<"3.Salir"<<endl;
+  cin >> opcion;
+  cout<<endl;
+
+  switch(opcion)
+  {
+    case 1: opcionManual(imagen_gris);
+            break;
+
+    case 2: opcionAuto();
+            break;
+
+    case 3:break;
+  }
+  }
+
+
 }
